0057293645b35df3745f9b5e9da8bc6da0cca098
Sunil Kumar Vadlamani <sunilteja93@users.noreply.github.com>
2019-02-04 23:13:34 -0800

    Update CalculateSumServerImpl.java

--- CalculateSumServerImpl.java
+++ CalculateSumServerImpl.java
@@ -2,18 +2,9 @@ import java.rmi.*;
 import java.rmi.server.*;
 
 public class CalculateSumServerImpl extends UnicastRemoteObject implements Project3ServerInterface {
-  public CalculateSumServerImpl() throws java.rmi.RemoteException
-  {}
+  public CalculateSumServerImpl() throws java.rmi.RemoteException {}
   public int calculateSum(long n) throws java.rmi.RemoteException
-    {
-     
-       /*
-        int res =0;        
-        for(int i = 0;i <= n; i++)
-        {
-           res = res+i;
-        }
-	*/    
+  {  
      long res;
      res = (n*(n+1))/2;
      return res;
